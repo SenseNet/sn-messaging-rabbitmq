f52c47affb0cde5df4c8fc1fda6d38306f70f8d5
tusmester <tusmester@gmail.com>
2023-11-03 13:25:26 +0100

    MArk classes with the options attribute.

--- src/SenseNet.Messaging.RabbitMQ/Configuration/RabbitMQ.cs
+++ src/SenseNet.Messaging.RabbitMQ/Configuration/RabbitMQ.cs
@@ -1,5 +1,8 @@
-﻿namespace SenseNet.Messaging.RabbitMQ.Configuration
+﻿using SenseNet.Tools.Configuration;
+
+namespace SenseNet.Messaging.RabbitMQ.Configuration
 {
+    [OptionsClass(sectionName: "sensenet:rabbitmq")]
     public class RabbitMqOptions
     {
         public string ServiceUrl { get; set; } = "amqp://localhost:5672";
